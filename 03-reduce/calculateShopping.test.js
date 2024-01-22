ff92cf3a2a71a15f6e001ece5a94610905e0362f
Zilong Zhan <144822890+ZilongZhan@users.noreply.github.com>
2024-01-22 20:04:21 +0100

    feat: define calculateShopping function and signature and initial tests

--- 03-reduce/calculateShopping.test.js
+++ 03-reduce/calculateShopping.test.js
@@ -1,3 +1,21 @@
-describe("", () => {
-  test("", () => {});
+import { calculateShopping } from "./calculateShopping.js";
+
+describe("given calculateShopping function", () => {
+  test("When given WISHES array as argument then expected integer should be returned", () => {
+    // Arrange
+    const WISHES = [
+      { title: "Tesla Model S", price: 90000 },
+      { title: "4 carat diamond ring", price: 45000 },
+      { title: "Fancy hacky Sack", price: 5 },
+      { title: "Gold fidgit spinner", price: 2000 },
+      { title: "A second Tesla Model S", price: 90000 },
+    ];
+
+    // Act
+    const result = calculateShopping(WISHES);
+
+    // Assert
+    expect(result).toBeDefined();
+    expect(result).toEqual(227005);
+  });
 });
